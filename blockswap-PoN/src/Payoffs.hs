38226fba885c697b8062a7a9c23eca3fff1da3d6
Philipp Zahn <philipp.m.zahn@gmail.com>
2023-04-20 09:07:03 +0200

    A bit of formatting

--- blockswap-PoN/src/Payoffs.hs
+++ blockswap-PoN/src/Payoffs.hs
@@ -14,7 +14,8 @@ Describes the payoffs for the different players
 
 -- Verify actions by reporter
 -- NOTE if no report has been filed, we default to _Nothing_
-verifyReport :: State -> ProposerAddr -> BuilderAddr -> SlotID -> SubmitReport AgentPenalized -> Maybe (ReportVerification AgentPenalized)
+verifyReport
+  :: State -> ProposerAddr -> BuilderAddr -> SlotID -> SubmitReport AgentPenalized -> Maybe (ReportVerification AgentPenalized)
 verifyReport state proposerAddr builderAddr slot report =
   case report of
      NoReport -> Nothing
@@ -33,32 +34,50 @@ verifyReport state proposerAddr builderAddr slot report =
 
 
 -- Check preconditions 
+preconditions
+  :: State -> ProposerAddr -> BuilderAddr -> SlotID -> Bool
 preconditions state proposerAddr builderAddr slot
-  | checkRegistered state proposerAddr == False = False -- ^ If not a registered validator, no fault on the validator's side
-  | checkRegistered state proposerAddr == True && checkPayment state slot builderAddr == True = False -- ^ Registered validator, payment realized
-  | checkRegistered state proposerAddr == True && checkPayment state slot builderAddr == False && checkDemand state slot == False = False -- ^ No payment realized for registered proposer but no demand -> all good
-  | checkRegistered state proposerAddr == True && checkPayment state slot builderAddr == False && checkDemand state slot == True = True  -- ^ Possibly proposer fault
+  | checkRegistered state proposerAddr == False    = False -- ^ If not a registered validator, no fault on the validator's side
+  | checkRegistered state proposerAddr == True
+    && checkPayment state slot builderAddr == True = False -- ^ Registered validator, payment realized
+  | checkRegistered state proposerAddr == True
+    && checkPayment state slot builderAddr == False
+    && checkDemand state slot == False             = False -- ^ No payment realized for registered proposer but no demand -> all good
+  | checkRegistered state proposerAddr == True
+    && checkPayment state slot builderAddr == False
+    && checkDemand state slot == True              = True  -- ^ Possibly proposer fault
 
 -- Check whether it is the proposer's fault (False == proposer not at fault, True == proposer at fault)
-verifyProposerFault :: State -> ProposerAddr -> BuilderAddr -> SlotID ->  Bool
+verifyProposerFault
+  :: State -> ProposerAddr -> BuilderAddr -> SlotID ->  Bool
 verifyProposerFault state@State{..} proposerAddr builderAddr slot
   | preconditions state proposerAddr builderAddr slot == False = False -- ^ proposer not at fault
-  | preconditions state proposerAddr builderAddr slot == True && checkBlocksForSlot state slot == True = True  -- ^ proposer grieving the relays
-  | preconditions state proposerAddr builderAddr slot == True && checkBlocksForSlot state slot == False && checkProposerRequest state slot == False = True  -- ^ proposer not having sent a request to at least one relay
-  | preconditions state proposerAddr builderAddr slot == True && checkBlocksForSlot state slot == False && checkProposerRequest state slot == True && checkProposerReplied state slot == False = True  -- ^ proposer not having sent a request to at least one relay
-  | otherwise = False -- ^ proposer not at faultslot  slot
+  | preconditions state proposerAddr builderAddr slot == True
+    && checkBlocksForSlot state slot == True                   = True  -- ^ proposer grieving the relays
+  | preconditions state proposerAddr builderAddr slot == True
+    && checkBlocksForSlot state slot == False
+    && checkProposerRequest state slot == False                = True  -- ^ proposer not having sent a request to at least one relay
+  | preconditions state proposerAddr builderAddr slot == True
+    && checkBlocksForSlot state slot == False
+    && checkProposerRequest state slot == True
+    && checkProposerReplied state slot == False                = True  -- ^ proposer not having sent a request to at least one relay
+  | otherwise                                                  = False -- ^ proposer not at faultslot  slot
 
 -- Check whether it is the builder's fault (False == builder not at fault, True == builder at fault)
-verifyBuilderFault :: State -> ProposerAddr -> BuilderAddr -> SlotID -> Bool
+verifyBuilderFault
+  :: State -> ProposerAddr -> BuilderAddr -> SlotID -> Bool
 verifyBuilderFault state proposerAddr builderAddr slot
-   | verifyProposerFault state proposerAddr builderAddr slot == False && checkPayment state slot builderAddr == False = True -- ^ If the proposer did everything right, but the payout pool still receives no money, it is the builder's fault
-   | verifyProposerFault state proposerAddr builderAddr slot == False && checkBuilderPayment state slot builderAddr == False = True -- ^ If the proposer behaved correctly and the payoutpool receives too little money, it is the builder's fault
-   | otherwise = False
+   | verifyProposerFault state proposerAddr builderAddr slot == False
+     && checkPayment state slot builderAddr == False                   = True -- ^ If the proposer did everything right, but the payout pool still receives no money, it is the builder's fault
+   | verifyProposerFault state proposerAddr builderAddr slot == False
+     && checkBuilderPayment state slot builderAddr == False            = True -- ^ If the proposer behaved correctly and the payoutpool receives too little money, it is the builder's fault
+   | otherwise                                                         = False
 
 -- Kick proposer for violating conditions (False == proposer not to be kicked; True == builder to be kicked)
 -- TODO: also check the time dimension; should this happen for current slots or later slots
 -- TODO: This needs to be checked
-verifyProposerKicking :: State -> ProposerAddr -> Bool
+verifyProposerKicking
+  :: State -> ProposerAddr -> Bool
 verifyProposerKicking State{..} proposerAddr
   | stakes < 32 = True
   | status == ProposerExited = True
@@ -69,8 +88,8 @@ verifyProposerKicking State{..} proposerAddr
 -- Reporter Payoff
 -- This uses the defined payoff parameters
 -- NOTE that we allow for penalities if reports are wrong
--- NOTE 
-payoffReporter :: ReporterPayoffParameters -> Maybe (ReportVerification AgentPenalized) -> Payoff
+payoffReporter
+  :: ReporterPayoffParameters -> Maybe (ReportVerification AgentPenalized) -> Payoff
 payoffReporter ReporterPayoffParameters{..} report
   | report == Just (ReportCorrect Validator)       = reportCorrectValidator
   | report == Just (ReportCorrect Builder)         = reportCorrectBuilder
