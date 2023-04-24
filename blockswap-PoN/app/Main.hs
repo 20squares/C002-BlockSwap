a2be4bfb1afec0b96beb060c204b5253fea9e188
Philipp Zahn <philipp.m.zahn@gmail.com>
2023-04-24 09:41:30 +0200

    Added missed payment

--- blockswap-PoN/app/Main.hs
+++ blockswap-PoN/app/Main.hs
@@ -15,12 +15,12 @@ import Types
 
 -- 1. main executable
 main  = do
-   -- putStrLn "Equilibrium Analytics: Honest Strategy"
-   -- printEquilibriumReporterGame parameters1 fullStrategyHonest
-   -- putStrLn "Equilibrium Analytics: False reporting strategy"
-   -- printEquilibriumReporterGame parameters1 fullStrategyFalse
-   -- putStrLn "Equilibrium Analytics: Honest Strategy -- too low payment"
-   -- printEquilibriumReporterGame parameters2 fullStrategyHonest
+   putStrLn "Equilibrium Analytics: Honest Strategy"
+   printEquilibriumReporterGame parameters1 fullStrategyHonest
+   putStrLn "Equilibrium Analytics: False reporting strategy"
+   printEquilibriumReporterGame parameters1 fullStrategyFalse
+   putStrLn "Equilibrium Analytics: Honest Strategy -- too low payment"
+   printEquilibriumReporterGame parameters2 fullStrategyHonest
    putStrLn "Equilibrium Analytics: Honest Strategy -- no payment"
    printEquilibriumReporterGame parameters3 fullStrategyHonest
 
